107ca22e5fb2828271d8e3a66429ab8c66a7ee55
Unknown <ThomasGiro@aol.com>
2018-11-21 18:00:40 +0100

    Option + Title Menu

---
+++ Assets/Scripts/UI/TitleMenu.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleMenu : MonoBehaviour {
+
+    Canvas canvas;
+    
+    public GameObject optionPrefab;
+    GameObject optionInstance = null;
+
+    void Awake () {
+        canvas = GetComponent<Canvas>();
+    }
+    
+    void Update() {
+        if(optionInstance != null)
+            canvas.enabled = false;
+        else
+            canvas.enabled = true;
+    }
+
+    public void ShowOptions() {
+        optionInstance = Instantiate(optionPrefab);
+    }
+
+    public void Play() {
+        // TODO
+    }
+
+}
