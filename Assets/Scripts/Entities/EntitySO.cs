bf0d59995bead6892b6adf843cdda2ce73b818a8
Althemar <damien.crechet@gmail.com>
2018-12-10 19:00:05 +0100

    Added resource to harvest to entity. Serializable dictionary accepts all

--- Assets/Scripts/Entities/EntitySO.cs
+++ Assets/Scripts/Entities/EntitySO.cs
@@ -2,6 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 using NaughtyAttributes;
+using System;
+using RotaryHeart.Lib.SerializableDictionary;
+
+[Serializable]
+public class ResourcesToHarvest : SerializableDictionaryBase<ResourceType, float>
+{
+}
 
 //[CreateAssetMenu(fileName = "Entity", menuName = "Entity", order = 1)]
 public class EntitySO : ScriptableObject
@@ -23,5 +30,8 @@ public class EntitySO : ScriptableObject
 
     [ReorderableList]
     public List<CustomTile> availableTiles;
+
+    [SerializeField]
+    public ResourcesToHarvest resources;
    
 }
