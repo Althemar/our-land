9d91e5306f0e3a519a55b2b89cdb23b552810e3c
Althemar <damien.crechet@gmail.com>
2018-12-13 13:11:00 +0100

    Wind emerges from holes

---
+++ Assets/Scripts/ResourceHarvestedUI.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class ResourceHarvestedUI : MonoBehaviour
+{
+    public Image image;
+    public TMP_Text text;
+
+    public float speed;
+    public float fadeSpeed;
+    public float offsetY;
+
+    public void Initialize(ResourceType resource, float count) {
+        image.sprite = resource.icon;
+        text.text = "+ " + count;
+        Vector3 newPosition = transform.position;
+        newPosition.y += offsetY;
+        transform.position = newPosition;
+    }
+
+    public void Update() {
+        Vector3 newPosition = transform.position;
+        newPosition.y += speed * Time.deltaTime;
+        transform.position = newPosition;
+
+        Color color = image.color;
+        color.a -= fadeSpeed * Time.deltaTime;
+        image.color = color;
+        text.color = color;
+
+        if (color.a <= 0) {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/Wind/WindOrigin.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WindOrigin : Updatable
+{
+    public HexDirection direction;
+    public int size = 2;
+    public int turnsBetweenSqualls = 3;
+
+    private int turnCount;
+    private TileProperties tile;
+
+    private bool producingWind;
+    private int currentSquallLength;
+
+    private Wind lastProduced;
+
+    private void Awake() {
+        turnCount = turnsBetweenSqualls;
+        AddToTurnManager();
+    }
+
+    private void Update() {
+        if (GameManager.Instance.FrameCount == 0) {
+            Vector3Int cellPosition = HexagonalGrid.Instance.Tilemap.WorldToCell(transform.position);
+            tile = HexagonalGrid.Instance.GetTile(new HexCoordinates(cellPosition.x, cellPosition.y));
+        }
+    }
+
+    public override void UpdateTurn() {
+        base.UpdateTurn();
+        if (turnCount == turnsBetweenSqualls && currentSquallLength < size) {
+            Wind newWind = WindManager.Instance.WindsPool.Pop();
+            newWind.InitializeChildWind(tile.GetNeighbor(direction), null, direction);
+            if (currentSquallLength > 0) {
+                for (int i = 0; i < lastProduced.next.Count; i++) {
+                    lastProduced.next[i].previous = newWind;
+                    newWind.next.Add(lastProduced.next[i]);
+                }
+                lastProduced.previous = newWind;
+            }
+            lastProduced = newWind;
+            currentSquallLength++;
+
+        }
+        else if (turnCount == turnsBetweenSqualls) {
+            currentSquallLength = 0;
+            turnCount = 1;
+        }
+        else {
+            turnCount++;
+        }
+        EndTurn();
+    }
+
+    public override void AddToTurnManager() {
+        TurnManager.Instance.AddToUpdate(this);
+    }
+
+    public override void RemoveFromTurnManager() {
+        TurnManager.Instance.RemoveFromUpdate(this);
+    }
+}
