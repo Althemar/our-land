be174c84e5b2e3f63e16b2bf602f19a25c2dae79
Althemar <damien.crechet@gmail.com>
2018-12-17 14:05:03 +0100

    Fix movement bug

--- Assets/Scripts/TileProperties.cs
+++ Assets/Scripts/TileProperties.cs
@@ -404,7 +404,7 @@ public class TileProperties : MonoBehaviour {
                 TileProperties[] neighbors = previousTile.GetNeighbors();
                 for (int j = 0; j < neighbors.Length; j++) {
                     TileProperties neighbor = neighbors[j];
-                    if (neighbor && !visited.Contains(neighbor) && neighbor.Tile.canWalkThrough && !neighbor.currentMovable && !neighbor.asLake) {
+                    if (neighbor && !visited.Contains(neighbor) && neighbor.Tile && neighbor.Tile.canWalkThrough && !neighbor.currentMovable && !neighbor.asLake) {
                         int distance = i - 1 + neighbor.Tile.walkCost;
                         if (distance <= movement) {
                             fringes[distance].Add(neighbor);
