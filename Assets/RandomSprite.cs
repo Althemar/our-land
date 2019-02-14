492db979a87bb2657630f6285741b35f0623db42
Althemar <damien.crechet@gmail.com>
2019-02-14 18:53:49 +0100

    Add mission to final scene

---
+++ Assets/RandomSprite.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomSprite : MonoBehaviour
+{
+    public List<Sprite> sprites;
+
+    private void Start() {
+        if (sprites.Count > 0) {
+            GetComponent<SpriteRenderer>().sprite = sprites[Random.Range(0, sprites.Count)];
+        }
+    }
+}
