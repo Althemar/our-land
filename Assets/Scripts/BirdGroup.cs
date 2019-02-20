5a7b07e8c6daa606fb164362ec82b6363529b22c
Althemar <damien.crechet@gmail.com>
2019-02-20 20:46:53 +0100

    Added birds

---
+++ Assets/Scripts/BirdGroup.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirdGroup : MonoBehaviour
+{
+
+    public delegate void OnBird();
+    public OnBird OnReachGoal;
+    public float speed = 2f;
+
+
+    private Vector3 spawn;
+    private Vector3 goal;
+
+    float progress = 0;
+
+    public void Initialize(Vector3 spawn, Vector3 goal, CameraControl cam) {
+        this.spawn = spawn;
+        this.goal = goal;
+        transform.position = spawn;
+        progress = 0;
+
+        foreach (Transform child in transform){
+            child.GetComponent<Bird>().Initialize(cam);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        progress += speed * Time.deltaTime;
+        transform.position = Vector3.Lerp(spawn, goal, progress);
+        if (progress >= 1) {
+            OnReachGoal?.Invoke();
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/Birds.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Birds : MonoBehaviour
+{
+    public BirdGroup birdPrefab;
+
+    public Transform spawn;
+    public Transform goal;
+
+    public CameraControl cam;
+    private Color tmpColor;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        NewBird();
+    }
+
+    public void NewBird() {
+        BirdGroup bird = Instantiate(birdPrefab, spawn.position, Quaternion.identity).GetComponent<BirdGroup>();
+        bird.Initialize(spawn.position, goal.position, cam);
+        bird.OnReachGoal += NewBird;
+    }
+}
