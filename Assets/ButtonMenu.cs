36ca0c6795761d3ece88035a03015cd7cc6d7f67
Althemar <damien.crechet@gmail.com>
2019-02-21 09:21:43 +0100

    begin menu

---
+++ Assets/ButtonMenu.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class ButtonMenu : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    public Menu menu;
+    public int index;
+    public bool locked = false;
+
+    public void OnPointerEnter(PointerEventData eventData) {
+        if (!locked) {
+            menu.ChangeSprite(index);
+        }
+    }
+
+    public void OnPointerExit(PointerEventData eventData) {
+        if (!locked) {
+            menu.ChangeSprite(0);
+        };
+    }
+}
---
+++ Assets/MainMenu.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+
+    private Menu currentMenu;
+
+    public static MainMenu Instance;
+
+    private void Start() {
+        Instance = this;
+    }
+
+    public void Play() {
+        SceneManager.LoadScene("Damien");
+    }
+
+    public void SetCurrentMenu(Menu menu) {
+        currentMenu = menu;
+    }
+
+    public void PreviousMenu() {
+        currentMenu.previous.gameObject.SetActive(true);
+        currentMenu.gameObject.SetActive(false);
+        currentMenu = currentMenu.previous;
+    }
+}
